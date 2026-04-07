d0fdd342714fe424bfeef2f9ece54f4e5e512a9f
josepagan <josepaganrnj@outlook.com>
2026-04-07 15:26:21 -0400

    1st Day of Java Coding Notes

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -4,14 +4,25 @@ package com.pluralsight;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+/*        This line prints out a greeting
+        this is another line of comment
+        you need to input /* and emd in */
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println("Yo, World!");
+// Declaring a variable x to the value of 1
+        int x = 1;
+        System.out.println(x);
+        int y;
+        y = 3;
+        System.out.println(x + y);
+        double w;
+        w = 3.3;
+        System.out.println(w);
+        String stillStrill = "Jose";
+        System.out.println(stillStrill);
+
+
+
+
     }
 }
